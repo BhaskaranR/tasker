a570538c4dd596dae3503fdf5cf75b245e290341
BHASKARAN RATHNACHALAM <bhaskaran@BHASKARANs-Mac-mini.local>
2018-11-02 16:54:23 -0400

    push

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bhaskaran/Documents/Source Code Remake/foodtasker_Task_15_Completed/foodtasker_Task_15_Completed/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
